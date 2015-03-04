862453a3a756a713715834b2ae9cc3782fdd5e12
rguaran <ritaguaran@gmail.com>
2015-03-04 12:32:26 -0600

    agregar resta

---
+++ cursac/src/main/java/com/cursarc/cursac/restar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursarc.cursac;
+
+/**
+ *
+ * @author RITA
+ */
+public class restar {
+    public restar(){}
+    
+    public int restar(int a, int b){
+    return a-b;
+    }
+    
+}
---
+++ cursac/src/test/java/com/cursarc/cursac/restarTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursarc.cursac;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author RITA
+ */
+public class restarTest {
+    
+    public restarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of restar method, of class restar.
+     */
+    @Test
+    public void testRestar() {
+        System.out.println("restar");
+        int a = 0;
+        int b = 0;
+        restar instance = new restar();
+        int expResult = 0;
+        int result = instance.restar(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
